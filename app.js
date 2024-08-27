16ccecba19bcc8d73cf55322fdb97928e37d69b6
hassansohail4u <hassansohail4u2@gmail.com>
2024-08-27 20:22:28 +0500

    JS Asignments completed Chapter 2

---
+++ app.js
@@ -0,0 +1,27 @@
+// Chapter 1
+
+// alert("Error! Please enter a valid password")
+// alert("Welcome to JS...")
+// alert("Happy Coding!\nPrevent this page from creating additional dialogs")
+console.log(alert("Hello... I can run JS through my web browser`s console"))
+
+// Chapter 2
+
+// var username = prompt("Enter your Name")
+// var age = prompt("Enter your Age")
+// var certified = prompt("Enter your Certified")
+
+
+// alert(username)
+// alert(age)
+// alert(certified)
+
+// var Dish = " PIZZA \n PIZZ \n PIZ \n PI \n P"
+
+// alert(Dish)
+
+// var email = "My email address is" +" "+"example@example.com"
+// alert(email)
+
+// var bookName = "I am trying to learn from the book A smarter\nway to learn JavaScript "
+// alert(bookName)
