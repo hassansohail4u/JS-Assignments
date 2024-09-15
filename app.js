5f5ac85bb5750ad73a5e25b8689631e5a5242d66
hassansohail4u <hassansohail4u2@gmail.com>
2024-09-16 01:23:01 +0500

    Chapter 1 to 5 Completed

--- app.js
+++ app.js
@@ -53,7 +53,11 @@
 // var displayMassage = document.querySelector("#display-massage")
 // displayMassage.innerHTML = "Yah! I can write HTML content through JavaScript"
 
-// 1 question remaining
+// for having this design visit this website
+// https://cutesymbols.me/rectangle.html
+// var design = "▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬"
+// alert(design)
+
 
 
 
@@ -200,16 +204,180 @@
 // var sum = +num1 / +num2
 // result.innerHTML = "Sum of " + num1 + " / " + num2 + " is " + sum
 
-var afterDeclarationhtml = document.querySelector("#after-declaration-html");
-var afterDeclaration = 
-afterDeclarationhtml.innerHTML = "Value after value declaration is " + afterDeclaration;
+// var afterDeclarationhtml = document.querySelector("#after-declaration-html");
+// var afterDeclaration = 
+// afterDeclarationhtml.innerHTML = "Value after value declaration is " + afterDeclaration;
 
-var initialValuehtml = document.querySelector("#initial-value-html");
-var initialValue = 5;
-initialValuehtml.innerHTML = "Initial Value : " + initialValue++ 
+// var initialValuehtml = document.querySelector("#initial-value-html");
+// var initialValue = 5;
+// initialValuehtml.innerHTML = "Initial Value: " + initialValue++ ;
 
-var afterIncrementhtml = document.querySelector("#after-increment-html")
-var afterIncrement = initialValue
-afterIncrementhtml.innerHTML = "Value after increment is : " + afterIncrement 
+// var afterIncrementhtml = document.querySelector("#after-increment-html");
+// var afterIncrement = initialValue;
+// afterIncrementhtml.innerHTML = "Value after increment is: " + afterIncrement;
 
-var hassanRaf = "hello"
+// var additionhtml = document.querySelector("#addition");
+// var addition = afterIncrement + 7;
+// additionhtml.innerHTML = "Value after addition is: " + addition--;
+
+// var decrementhtml = document.querySelector("#decrement");
+// var decrement = addition;
+// decrementhtml.innerHTML = "Value after Decrement is: " + decrement;
+
+// var reminderhtml = document.querySelector("#reminder");
+// var reminder = decrement%3;
+// reminderhtml.innerHTML = "The Remainder is: " + reminder;
+
+// var result = document.querySelector("#result")
+// var ticketPrice = 600;
+// var userQuantity = prompt("Enter The Ticket Quantity")
+
+// var totalPrice = ticketPrice*userQuantity
+// result.innerHTML = "Total cost to buy " + userQuantity + " Tickets to a movie is " + totalPrice + " PKR"
+
+
+
+
+
+// var enterNum = prompt("Enter Number for Table")
+
+// var tableresult1 = document.querySelector("#tableresult1")
+// var tableresult2 = document.querySelector("#tableresult2")
+// var tableresult3 = document.querySelector("#tableresult3")
+// var tableresult4 = document.querySelector("#tableresult4")
+// var tableresult5 = document.querySelector("#tableresult5")
+// var tableresult6 = document.querySelector("#tableresult6")
+// var tableresult7 = document.querySelector("#tableresult7")
+// var tableresult8 = document.querySelector("#tableresult8")
+// var tableresult9 = document.querySelector("#tableresult9")
+// var tableresult10 = document.querySelector("#tableresult10")
+
+// var numTable1 = enterNum + " x " + 1 + " = " + enterNum*1
+// var numTable2 = enterNum + " x " + 2 + " = " + enterNum*2
+// var numTable3 = enterNum + " x " + 3 + " = " + enterNum*3
+// var numTable4 = enterNum + " x " + 4 + " = " + enterNum*4
+// var numTable5 = enterNum + " x " + 5 + " = " + enterNum*5
+// var numTable6 = enterNum + " x " + 6 + " = " + enterNum*6
+// var numTable7 = enterNum + " x " + 7 + " = " + enterNum*7
+// var numTable8 = enterNum + " x " + 8 + " = " + enterNum*8
+// var numTable9 = enterNum + " x " + 9 + " = " + enterNum*9
+// var numTable10 = enterNum + " x " + 10 + " = " + enterNum*10
+
+// tableresult1.innerHTML = numTable1
+// tableresult2.innerHTML = numTable2
+// tableresult3.innerHTML = numTable3
+// tableresult4.innerHTML = numTable4
+// tableresult5.innerHTML = numTable5
+// tableresult6.innerHTML = numTable6
+// tableresult7.innerHTML = numTable7
+// tableresult8.innerHTML = numTable8
+// tableresult9.innerHTML = numTable9
+// tableresult10.innerHTML = numTable10
+
+
+
+
+// var result = document.querySelector("#result")
+// var celcius = 50;
+// var fahrenheit = (celcius * 9/5 ) + 32;
+// result.innerHTML = ( celcius +"% is " + fahrenheit + "%");
+
+
+
+// var item1 = document.querySelector("#item-1")
+// var quantityitem1 = document.querySelector("#quantity-item-1")
+// var item2 = document.querySelector("#item-2")
+// var quantityitem2 = document.querySelector("#quantity-item-2")
+// var shipCharges = document.querySelector("#ship-charges")
+
+// var priceitem1  = "Price of item 1 is " + 650;
+// var item1quantity  = "Quantity of item 1 is " + 3;
+// var priceitem2 = "Price of item 1 is " + 100;
+// var item2quantity = "Quantity of item 1 is "+ 7;
+// var charges = "Shipping Charges " + 100;
+// var totalitem1 =  priceitem1*item1quantity 
+// var totalitem2 = priceitem2*item2quantity
+// var total = 1950 + 700 + 100
+
+// item1.innerHTML = priceitem1;
+// quantityitem1.innerHTML = item1quantity;
+// item2.innerHTML = priceitem2;
+// quantityitem2.innerHTML = item2quantity;
+// shipCharges.innerHTML = charges;
+
+
+// result.innerHTML = "Total cost of your order is " + total
+
+
+// var obtain = document.querySelector("#obtain-marks")
+// var total = document.querySelector("#total-marks")
+// var percentagehtml = document.querySelector("#percentage")
+
+// var userobtain = prompt("Enter your obtain marks")
+// var totalmarks = 980;
+// var percentage = userobtain/totalmarks*100
+
+// total.innerHTML = "Total Marks : " + totalmarks;
+// obtain.innerHTML = "Marks Obtained : " + userobtain;
+// percentagehtml.innerHTML = "Percentage : " + percentage;
+
+// var result = document.querySelector("#result")
+// var dollar = 10*104.80;
+// var riyal = 25*28;
+// var total = dollar + riyal
+
+// result.innerHTML = "Total Currency in PKR: " + total 
+
+
+
+// var result = document.querySelector("#result")
+// var total = (5*10)/2
+
+// result.innerHTML = total
+
+
+
+// var result = document.querySelector("#result")
+// var useryear = prompt("Enter Your Birth Year")
+// var currentyear = 2024;
+// var total = currentyear - useryear
+
+// result.innerHTML = total
+
+
+
+
+// var radius = document.querySelector("#radius")
+// var circumhtml = document.querySelector("#circum")
+// var areahtml = document.querySelector("#area")
+// var circleRadius = 20
+// var circum = -2*circleRadius*-3.142;
+// var area = -20*20*-3.142
+
+// radius.innerHTML = "Radius of a Circle: " + circleRadius;
+// circumhtml.innerHTML = "The circumference is: " + circum;
+// areahtml.innerHTML = "The area is: " + area
+
+
+
+
+
+
+var favouriteSnackhtml = document.querySelector("#favourite-snack")
+var currentagehtml = document.querySelector("#current-age")
+var estiagehtml = document.querySelector("#esti-age")
+var perdayhtml = document.querySelector("#per-day")
+var result = document.querySelector("#result")
+
+var favourite = "RIO Biscuit";
+var currentage = 15;
+var estiAge = 65;
+var perDay = 4;
+var agetotal = estiAge-currentage;
+var total = perDay*agetotal;
+
+favouriteSnackhtml.innerHTML = "Favourite Snack: " + favourite
+currentagehtml.innerHTML = "Current Age: " + currentage
+estiagehtml.innerHTML = "Estimated Maximum Age: " + estiAge
+perdayhtml.innerHTML = "Amount of snacks per day: " + perDay
+result.innerHTML = "You will need " + total + " " + favourite + " to last you until the ripe old age of " + estiAge 
